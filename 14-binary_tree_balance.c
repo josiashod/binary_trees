7ca5653c23d167d80e886252df9895b21f4859cc
josiashod <djossoujosiashod@gmail.com>
2022-08-23 23:22:53 +0100

    update code

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -13,8 +13,8 @@ int tree_height(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	l_height = tree->left ? 1 + tree_height(tree->left) : -1;
-	r_height = tree->right ? 1 + tree_height(tree->right) : -1;
+	l_height = tree->left ? 1 + tree_height(tree->left) : 1;
+	r_height = tree->right ? 1 + tree_height(tree->right) : 1;
 	return (max(l_height, r_height));
 }
 
@@ -30,5 +30,5 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	return (tree_height(tree->left) + tree_height(tree->right));
+	return (tree_height(tree->left) - tree_height(tree->right));
 }
