fc8a0cb79108ba947e9a6f57dcd9bd329fff7fc3
josiashod <djossoujosiashod@gmail.com>
2022-08-24 20:26:04 +0100

    Task 19

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -16,9 +16,11 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 
 	if (first == second)
 		return ((binary_tree_t *) first);
-	if (first == second->parent || !first->parent)
-                return (binary_trees_ancestor(first, second->parent));
-	else if (first->parent == second || !second->parent)
+	if (first == second->parent || !first->parent ||
+	    (!first->parent->parent && second->parent))
+		return (binary_trees_ancestor(first, second->parent));
+	else if (first->parent == second || !second->parent ||
+		(!second->parent->parent && first->parent))
 		return (binary_trees_ancestor(first->parent, second));
 	return (binary_trees_ancestor(first->parent,
 				      second->parent));
