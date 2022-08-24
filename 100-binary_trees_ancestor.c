f72dea225c3e3864f43f8faacc5dcdcc913f3657
josiashod <djossoujosiashod@gmail.com>
2022-08-24 19:53:57 +0100

    Update binary_trees_ancestor function

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -13,21 +13,18 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 {
 	if (first && second)
 	{
-		if (first->parent && second->parent)
-		{
-			if (first->parent == second->parent)
-				return (first->parent);
-			else if (first == second->parent)
-				return (second->parent);
-			else if (second == first->parent)
-				return (first->parent);
-			else if (first->parent->parent == second->parent)
-				return (second->parent);
-			else if (first->parent == second->parent->parent)
-				return (first->parent);
-			return (binary_trees_ancestor(first->parent,
-						      second->parent));
-		}
+		if (first->parent == second->parent)
+			return (first->parent);
+		else if (first == second->parent)
+			return (second->parent);
+		else if (second == first->parent)
+			return (first->parent);
+		else if (first->parent->parent == second->parent)
+			return (second->parent);
+		else if (first->parent == second->parent->parent)
+			return (first->parent);
+		return (binary_trees_ancestor(first->parent,
+							second->parent));
 	}
 	return (NULL);
 }
